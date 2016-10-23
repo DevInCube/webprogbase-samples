265482a0b78cfa6ddd687989be1e0cae949c1b77
PublicHadyniak <public.hadyniak@gmail.com>
2016-10-23 16:38:36 +0300

    web-frameworks sample VueJS

--- clientjs/items_vuejs.js
+++ clientjs/items_vuejs.js
@@ -1,30 +1,93 @@
-new Vue({
-	el: '#users',
-	data: {
-		users: [
-			{
-				'fullname': 'Taras Sheva'
+window.addEventListener('load', function() {
+	new Vue({
+		el: '#users',
+		data: {
+			users: [],
+			nameFilter: ""
+		},
+		computed: {
+			filteredUsers: function () {
+				var filterString = this.nameFilter.toLowerCase().trim();
+				if (this.nameFilter) {
+					return this.users.filter(x => x.fullname.toLowerCase().includes(filterString));
+				} else {
+					return this.users;
+				}
+			}
+		},
+		watch: {
+			nameFilter: function(val) {
+				console.log(val);
+			}
+		},
+		methods: {
+			fetchUsers: function() {
+				fakeAjaxGetUsers()
+					.then(users => this.users = users)
+					.catch(err => console.log("Can't fetch users from server"));
 			},
-			{
-				'fullname': 'Some Dude'
+			clearFilter: function() {
+				this.nameFilter = "";
 			},
-			{
-				'fullname': 'Taras Shevchenko'
+			createUser: function(event) {
+				event.preventDefault();
+				var newUserName = event.target.newusername.value;
+				fakeAjaxCreateUser(newUserName)
+					.then(user => this.users.push(user))
+					.catch(err => console.log("Can't create user on server"));
 			},
-			{
-				'fullname': 'Inna Baa'
-			},
-			{
-				'fullname': 'Taras Taras'
-			},
-		],
-		nameFilter: "",
-		filteredUsers: this.users
-	},
-	watch: {
-		nameFilter: function(val) {
-			console.log(val);
-			this.filteredUsers = this.users.filter(x => x.fullname.includes(this.filter));
+			removeUser: function(event) {
+				var userId = event.target.attributes.user_id.value;
+				fakeAjaxRemoveUser(userId)
+					.then(x => this.users.splice(this.users.findIndex(x => x.id == userId), 1))
+					.catch(err => console.log("Can't remove user on server"));
+			}
+		},
+		created: function() {
+			console.log("vue is created. Fetching users");
+			this.fetchUsers();
 		}
-	}
+	});
 });
+
+function fakeAjaxCreateUser(username) {
+	console.log("Create user on server");
+	return Promise.resolve({
+		id: 10 + (Math.random() * 100000) | 0,
+		fullname: username
+	});
+};
+
+function fakeAjaxRemoveUser(userId) {
+	console.log("Remove user with id " + userId + " on server");
+	return Promise.resolve(true);
+}
+
+function fakeAjaxGetUsers() {
+	return Promise.resolve([
+		{
+			id: 1,
+			fullname: 'Taras Sheva'
+		},
+		{
+			id: 2,
+			fullname: 'Some Dude'
+		},
+		{
+			id: 3,
+			fullname: 'Taras Shevchenko'
+		},
+		{
+			id: 4,
+			fullname: 'Inna Baa'
+		},
+		{
+			id: 5,
+			fullname: 'Taras Taras'
+		},
+		{
+			id: 6,
+			fullname: 'Sash Kaa'
+		}
+	]);
+}
