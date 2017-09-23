b8d127a0aa42eb3e96f744fee9c1a65f6abfe950
publichadyniak <public.hadyniak@gmail.com>
2017-09-23 16:16:07 +0300

    images

--- express_ejs/app.js
+++ express_ejs/app.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const busboyBodyParser = require('busboy-body-parser');
 
 let app = express();
 
@@ -7,19 +8,50 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(busboyBodyParser());  // for files
 
-// let usersRouter = require('./routes/users');
-// app.use('/users', usersRouter);
+let user = {
+	name: 'User',
+	age: 11,
+	sex: 'male',
+	avaname: null
+};
 
-app.get('/', 
-	(req, res) => res.render('index'));
-app.post('/', (req, res) => {
-	let name = req.body.name;
-	res.json({name});
+//
+// images dictionary
+let images = {
+
+};
+
+let sexes = [
+	{ value: 'female', title: 'Female' },
+	{ value: 'male', title: 'Male' },
+	{ value: 'apache', title: 'Apache!' },
+]
+
+app.get('/',
+	(req, res) => res.render('index', {
+		user,
+		sexes
+	}));
+
+app.post('/user/update', (req, res) => {
+	user.name = req.body.name;
+	user.age = req.body.age;
+	user.sex = req.body.sex;
+	 // console.log(req.files);
+	if (req.files.ava) {
+		let filename = req.files.ava.name;
+		images[filename] = req.files.ava;
+		user.avaname = filename;
+	}
+	res.redirect('/');
 });
 
-app.use((req, res) => {
-	res.send('hello');
+//
+// to send images (as bytes)
+app.get("/images/:name", (req, res) => {
+	res.send(images[req.params.name].data);
 });
 
 app.listen(3000, () => console.log("UP!"));
